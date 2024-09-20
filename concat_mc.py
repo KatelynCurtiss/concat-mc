7eba942a30fde83b5819d94b6ccdb0ccd3f757f7
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-09-20 11:05:55 -0400

    Update concat_mc.py

--- concat_mc.py
+++ concat_mc.py
@@ -18,3 +18,11 @@ string1 = ("What I've learned about concatination. ")
 
 string2 = ("It's harder to read than strings. ")
 string3 = ("It's easier write out,but it has promblem to spacing. ")
+
+# Task 3
+
+resterantname = (input ("Resterant: ") )
+price = str(input ("Price: "))
+
+print("At " + resterantname + ", a book set cost " + price + ".")
+
