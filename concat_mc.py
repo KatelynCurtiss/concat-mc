7c9cf6da341dd9555a7bb94452ee97153a762d81
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-09-20 10:54:20 -0400

    Create concat_mc.py

---
+++ concat_mc.py
@@ -0,0 +1,20 @@
+# Katelyn Curtiss
+# Setember 20 2024
+# Concatination Check
+
+
+# Task 1
+myname = ("Katelyn Curtiss. ")
+
+favoritegame = ("Twisted Wonderland is my favorite game. ")
+
+
+
+# Task 2
+print(myname)
+print(favoritegame)
+
+string1 = ("What I've learned about concatination. ")
+
+string2 = ("It's harder to read than strings. ")
+string3 = ("It's easier write out,but it has promblem to spacing. ")
