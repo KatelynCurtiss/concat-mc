6375760f1948c1e9ee90adcf59ce4094ca7874da
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-09-20 11:18:22 -0400

    Update concat_mc.py

--- concat_mc.py
+++ concat_mc.py
@@ -1,23 +1,23 @@
 # Katelyn Curtiss
 # Setember 20 2024
-# Concatination Check
+# Concatenation Check
 
 
 # Task 1
-myname = ("Katelyn Curtiss. ")
+myname = "Katelyn Curtiss. "
 
-favoritegame = ("Twisted Wonderland is my favorite game. ")
+favoritegame = "Twisted Wonderland"
 
-
+full_sentence = "My name is " + myname + ", my favorite game is " + favoritegame + "."
 
 # Task 2
 print(myname)
 print(favoritegame)
 
-string1 = ("What I've learned about concatination. ")
+string1 = "What I've learned about concatination. "
 
-string2 = ("It's harder to read than strings. ")
-string3 = ("It's easier write out,but it has promblem to spacing. ")
+string2 = "It's harder to read than strings. "
+string3 = "It's easier write out,but it has promblem to spacing. "
 
 # Task 3
 
